5607a732ca88bd944e206a2014ee296c2c29692e
Rohit Jha <me@rjha.dev>
2023-01-21 22:58:50 +0530

    fix: looping

--- src/components/pages/schemetrav.js
+++ src/components/pages/schemetrav.js
@@ -1,22 +1,20 @@
 import './pages.css'
 import shData from './scheme-data';
-import React,{Component } from 'react'
-    function MapDemo(){
-        const map1 = shData.map(item => item.schTitle)
-        const map2 = shData.map(item => item.schArea)
-        const map3 = shData.map(item => item.schContent)
-        
-        
-        return(
-            <>
-            {shData.data.map((e, count) => {
-                <h1> {e.schTitle}</h1>
-            }
-            )};
+import React, { Component } from 'react'
+function MapDemo() {
+    return (
+        <>
+            {shData.map(e => {
+                return (
+                    <>
+                        <h1>{e.schTitle}</h1>
+                        <p>{e.schArea}</p>
+                        <p>{e.schContent}</p>
+                    </>
+                );
 
-            
-            
-            </>
-        )
-    }
+            })}
+        </>
+    )
+}
 export default MapDemo;
